cc11c40387fc52d8e1be66147151eb48d8f651b9
Matteo Vidor <matteo.vidor@etu.unilim.fr>
2026-03-24 12:42:37 +0100

    Premier commit projet MAUI

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MatteoAPP1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
